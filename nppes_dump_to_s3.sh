5f7f89f95acec588d6ca7dd439a129f98cd0f989
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-11-29 14:36:25 -0500

    Update nppes_dump_to_s3.sh

--- nppes_dump_to_s3.sh
+++ nppes_dump_to_s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+echo "hello"
 mkdir dump
 mv *.txt dump
-
-aws s3 sync dump/ s3://nppes2/dump-"$(date +%d-%m)"/
+aws s3 sync dump/ s3://viquity-database-import-us-east-1/Jobs/nppes/dump-"$(date +%d-%m-%y-%H-%M)"/
