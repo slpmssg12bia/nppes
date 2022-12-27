8d09cfa54ea77186d0d842efb5b26604b43d3343
slpmssg12bia <102541001+slpmssg12bia@users.noreply.github.com>
2022-12-27 09:19:24 -0500

    Update dump_to_s3.sh

--- dump_to_s3.sh
+++ dump_to_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir nppesdump
 mv *.csv  *.pdf nppesdump
-aws s3 sync nppesdump/ s3://viquity-database-import-us-east-1/Jobs/nppes/nppesdump/
+aws s3 sync nppesdump/ s3://viquity-database-import-us-east-1/Jobs/nppes/nppes_current_dump/nppesdump/
